3acb836761d039579cf2d6fa2488f2848f18d0ba
Antony Baasandorj <antony.baasan@gmail.com>
2021-11-28 05:18:11 -0500

    add metadata model

--- server/Moralis/MoralisService.cs
+++ server/Moralis/MoralisService.cs
@@ -28,21 +28,35 @@ namespace Moralis
     public record MoralisNftResult(int total, int page, int page_size, string status, List<Dictionary<string, string>> result);
     public record MoralisNftObject
     {
-        string token_address { get; set; } = string.Empty;
-        string token_id { get; set; } = string.Empty;
-        string amount { get; set; } = string.Empty;
-        string owner_of { get; set; } = string.Empty;
-        string block_number { get; set; } = string.Empty;
-        string block_number_minted { get; set; } = string.Empty;
-        string contract_type { get; set; } = string.Empty;
-        string name { get; set; } = string.Empty;
-        string symbol { get; set; } = string.Empty;
-        string token_uri { get; set; } = string.Empty;
-        string metadata { get; set; } = string.Empty;
-        int isValid { get; set; }
-        int syncing { get; set; }
-        DateTime synced_at { get; set; }
-        int frozen { get; set; }
+        public string Token_address { get; set; } = string.Empty;
+        public string Token_id { get; set; } = string.Empty;
+        public string Amount { get; set; } = string.Empty;
+        public string Owner_of { get; set; } = string.Empty;
+        public string Block_number { get; set; } = string.Empty;
+        public string Block_number_minted { get; set; } = string.Empty;
+        public string Contract_type { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Symbol { get; set; } = string.Empty;
+        public string Token_uri { get; set; } = string.Empty;
+        public int IsValid { get; set; }
+        public int Syncing { get; set; }
+        public int Frozen { get; set; }
+        public DateTime Synced_at { get; set; }
+        public List<MoralisNftMetadata> Metadata { get; set; } = new List<MoralisNftMetadata>();
+    }
+
+    public record MoralisNftMetadata
+    {
+        public string ImageUrl { get; set; } = string.Empty;
+        public string Ipfs { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public List<MoralisNftProperty> Properties { get; set; } = new List<MoralisNftProperty>();
+    }
+    public record MoralisNftProperty
+    {
+        public string Trait_type { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
     }
 
 }
