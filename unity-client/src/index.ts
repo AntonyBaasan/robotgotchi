44227b4948a95c0a0a8048fdfab7acc9373e8c38
Antony Baasandorj <antony.baasan@gmail.com>
2021-11-30 21:43:50 -0500

    add unity client project

---
+++ unity-client/src/index.ts
@@ -0,0 +1,10 @@
+import { Utils } from './utils';
+
+export class Robotgotchi {
+
+    public Hi(): void {
+        const u = new Utils();
+
+        console.log("Hello World my friend!");
+    }
+}
---
+++ unity-client/src/utils.ts
@@ -0,0 +1,5 @@
+export class Utils {
+    public sayBye() {
+        console.log('Bye');
+    }
+}
